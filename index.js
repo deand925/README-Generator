1a6599185ff2b3033a98740abb9aac33d1a97ba2
deand925 <97762768+deand925@users.noreply.github.com>
2022-09-14 19:00:05 -0700

    finished inex.js file/ creating readme

--- index.js
+++ index.js
@@ -52,12 +52,30 @@ const questions = [
     }
 ];
 
-
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+    fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions
+    /* Pass your questions in here */
+  )
+  .then((answers) => {
+      let markdown = generateMarkdown(answers);
+        writeToFile('README.md', markdown)
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+}
 
 // Function call to initialize app
 init();
