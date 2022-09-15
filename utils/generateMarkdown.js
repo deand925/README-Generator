4193e8c2f8525b2206116aaf31fad832852ebbd8
deand925 <97762768+deand925@users.noreply.github.com>
2022-09-14 19:01:59 -0700

    created markdown section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,10 +11,63 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
+
+  ## Table of Contents
+  
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Description
+ 
+  ${data.description}
+
+  ## Installation
+  
+  ${data.installation}
+
+  ## Usage
+  
+  ${data.usage}
+  
+  
+  ${renderLicenseSection(data.license)}
+  
+  ${renerLicense(data.license)}
+
+  ---
+
+  ## Contributing
+
+  ${data.credits}
+  
+  ## Images
+
+  <img src="images/Screen Shot 2022-08-24 at 9.26.55 AM.png">
+
+  ## Tests
+
+  ${data.test}
+
+  ## Questions
+
+  Please contact me using the following links:
+
+  [Email: ${data.email}](mailto:${data.email})
+
+  [GitHub](https://github.com/${data.github})
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+
+
