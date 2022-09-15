938c5f9862c8966495690bc5d4af83cb80bcd92c
deand925 <97762768+deand925@users.noreply.github.com>
2022-09-14 18:54:44 -0700

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your README?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter what you would like in your description"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use."
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "List your collaborators, if any, with links to their GitHub profiles."
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license will you use for your project?',
+        choices: ['apache','boost', 'BSD 3-Clause License','BSD 2-Clause License', 'Eclipse Public License 1.0', 'GNU GPL v3', 'no license']
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is you email address?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide any test instructions you want to include."
+    }
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
